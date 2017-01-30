3f245c4a74ec14d16e009ed339e7e4fc8d8aca36
joellegg <joellegg@bellsouth.net>
2017-01-30 14:35:58 -0600

    added jshint package

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,24 +1,34 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    sass: {
-      options: {
-        sourceMap: true,
-        outputStyle: 'compressed'
-      },
-      dist: {
-        files: {
-          'styles/main.css': 'styles/main.scss'
-        }
-      },
-    },
-  });
+    // Project configuration.
+    grunt.initConfig({
+        sass: {
+            options: {
+                sourceMap: true,
+                outputStyle: 'compressed'
+            },
+            dist: {
+                files: {
+                    'styles/main.css': 'styles/main.scss'
+                }
+            },
+        },
+        watch: {
+            sass: {
+                files: ['**/*.scss'],
+                tasks: ['sass'],
+            },
+        },
+        jshint: {
+            all: ['main.js']
+        },
+    });
 
-  // Load the Grunt plugin that provides the "sass" task.
-  grunt.loadNpmTasks('grunt-sass');
+    // Load the Grunt plugin that provides the "sass" task.
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
 
-  // Default task(s).
-  grunt.registerTask('default', ['sass']);
-
+    // Default task(s).
+    grunt.registerTask('default', ['sass', 'jshint']);
 };
