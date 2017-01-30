a9c83d107127b740a078e88988d66210d0bb0a7d
joellegg <joellegg@bellsouth.net>
2017-01-30 14:36:42 -0600

    gf to 2 spaces

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,34 +1,34 @@
 module.exports = function(grunt) {
 
-    // Project configuration.
-    grunt.initConfig({
-        sass: {
-            options: {
-                sourceMap: true,
-                outputStyle: 'compressed'
-            },
-            dist: {
-                files: {
-                    'styles/main.css': 'styles/main.scss'
-                }
-            },
-        },
-        watch: {
-            sass: {
-                files: ['**/*.scss'],
-                tasks: ['sass'],
-            },
-        },
-        jshint: {
-            all: ['main.js']
-        },
-    });
+  // Project configuration.
+  grunt.initConfig({
+    sass: {
+      options: {
+        sourceMap: true,
+        outputStyle: 'compressed'
+      },
+      dist: {
+        files: {
+          'styles/main.css': 'styles/main.scss'
+        }
+      },
+    },
+    watch: {
+      sass: {
+        files: ['**/*.scss'],
+        tasks: ['sass'],
+      },
+    },
+    jshint: {
+      all: ['main.js']
+    },
+  });
 
-    // Load the Grunt plugin that provides the "sass" task.
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-sass');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
+  // Load the Grunt plugin that provides the "sass" task.
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
-    // Default task(s).
-    grunt.registerTask('default', ['sass', 'jshint']);
+  // Default task(s).
+  grunt.registerTask('default', ['sass', 'jshint']);
 };
