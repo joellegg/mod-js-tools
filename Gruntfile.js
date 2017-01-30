c927ed047263886591eb28b74e4ca785e86ac065
joellegg <joellegg@bellsouth.net>
2017-01-30 14:43:44 -0600

    added fake js

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,5 +30,5 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
 
   // Default task(s).
-  grunt.registerTask('default', ['sass', 'jshint']);
+  grunt.registerTask('default', ['sass', 'grunt-contrib-jshint']);
 };
--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+var listLink = $("#link-list");
+var listView = $("#list-view");
 
+listLink.click(function(event) {
+    event.preventDefault();
+
+    listView.addClass("visible");
+    listView.removeClass("hidden");
+});
